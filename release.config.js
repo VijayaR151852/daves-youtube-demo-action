70e3cc76916ddfa01c0d4b1a1594bdca4c171128
vijayalakshmimule <lakshmi210800@gmail.com>
2025-04-22 02:42:24 +0530

    fix: adds repo url

--- release.config.js
+++ release.config.js
@@ -1,14 +1,13 @@
 module.exports = {
   branches: ['main'],
+  repositoryUrl: 'https://github.com/VijayaR151852/daves-youtube-demo-action.git',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ["@semantic-release/git", {
-      "assets": ["dist/*.js", "dist/*.js.map"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    ['@semantic-release/git', {
+      assets: ['dist/*.js', 'dist/*.js.map'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
     }],
     '@semantic-release/github'
   ]
 };
-
-
